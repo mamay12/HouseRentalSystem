a7c576935a913555b3bb4531654b2a3248f92552
Yaroslav Tymchenko <ytimchenko57@gmail.com>
2025-03-25 20:53:24 +0200

    init zudoku app

---
+++ zudoku-app/zudoku.config.ts
@@ -0,0 +1,28 @@
+import type { ZudokuConfig } from "zudoku";
+
+const config: ZudokuConfig = {
+  topNavigation: [
+    { id: "docs", label: "Documentation" },
+    { id: "api", label: "API Reference" },
+  ],
+  sidebar: {
+    docs: [
+      {
+        type: "category",
+        label: "Overview",
+        items: ["docs/installation", "docs/authorization", "docs/about"],
+      },
+    ],
+  },
+  redirects: [{ from: "/", to: "/docs/installation" }],
+  apis: {
+    type: "file",
+    input: "./apis/openapi.json",
+    navigationId: "api",
+  },
+  docs: {
+    files: "/pages/**/*.{md,mdx}",
+  },
+};
+
+export default config;
