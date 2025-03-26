efecae490be95763a72b5ab248f680938e3ce1c2
Yaroslav Tymchenko <ytimchenko57@gmail.com>
2025-03-26 18:39:34 +0200

    try fix

--- docs/zudoku-app/zudoku.config.ts
+++ docs/zudoku-app/zudoku.config.ts
@@ -1,7 +1,7 @@
 import type {ZudokuConfig} from "zudoku";
 
 const config: ZudokuConfig = {
-    basePath:"HouseRentalSystem",
+    basePath:"/HouseRentalSystem",
     topNavigation: [
         {id: "docs", label: "Documentation"},
         {id: "api", label: "API Reference"},
