ff4648e4b3405b92a6d452bbad80af21e33e524c
Yaroslav Tymchenko <ytimchenko57@gmail.com>
2025-03-26 17:14:28 +0200

    try fix

--- docs/zudoku-app/zudoku.config.ts
+++ docs/zudoku-app/zudoku.config.ts
@@ -1,7 +1,6 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
-  basePath:'/HouseRentalSystem',
   topNavigation: [
     { id: "docs", label: "Documentation" },
     { id: "api", label: "API Reference" },
@@ -15,7 +14,7 @@ const config: ZudokuConfig = {
       },
     ],
   },
-  redirects: [{ from: "/", to: "/docs/installation" }],
+  // redirects: [{ from: "/", to: "/docs/installation" }],
   apis: {
     type: "file",
     input: "./apis/openapi.json",
